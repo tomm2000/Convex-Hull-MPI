6490c3d533cff0a430110df458e921ae7867f9f4
tomm2000 <38072918+tomm2000@users.noreply.github.com>
2024-07-12 15:56:24 +0200

    cleanup

--- QuickHullMPI.cxx
+++ QuickHullMPI.cxx
@@ -22,10 +22,6 @@ int main(int argc, char *argv[]) {
   // Get the number of processes
   int numP;
   MPI_Comm_size(MPI_COMM_WORLD, &numP);
-  // Get processor name
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int namelen;
-  MPI_Get_processor_name(processor_name, &namelen);
   // Get the ID of the process
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
